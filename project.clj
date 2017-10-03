ef258154c8c5001a1085c06e5babfe8159f93f17
Ivan Kuznetsov <me@jeiwan.ru>
2017-10-03 09:26:04 +0700

    Use cljsbuild to compile and run tests

--- project.clj
+++ project.clj
@@ -4,8 +4,16 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.2"]]
+  :plugins [[lein-npm "0.6.2"]
+            [lein-cljsbuild "1.1.7"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
-  :target-path "target")
+  :target-path "target"
+  :cljsbuild {:builds {:test {:source-paths ["src" "test"]
+                              :compiler {:output-to "out/test/runner.js"
+                                         :target :nodejs
+                                         :main cljs-testing.tests-runner
+                                         :optimizations :simple
+                                         :pretty-print true}}}
+              :test-commands {"test" ["node" "out/test/runner.js"]}})
