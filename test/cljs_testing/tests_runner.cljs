cb99144aa91d24c172333fb9e216a5ed15a93a35
Ivan Kuznetsov <me@jeiwan.ru>
2017-10-04 08:13:17 +0700

    Remove a comment

--- test/cljs_testing/tests_runner.cljs
+++ test/cljs_testing/tests_runner.cljs
@@ -1,8 +1,6 @@
 (ns cljs-testing.tests-runner
   (:require  [cljs.test :refer [run-all-tests run-tests]]
-             ;; [cljs-testing.core-test :as core-test]
-             cljs-testing.core-test
-             ))
+             cljs-testing.core-test))
 
 (enable-console-print!)
 
